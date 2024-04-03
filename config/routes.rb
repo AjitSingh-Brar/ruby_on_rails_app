0f16c3b1adc18042220d12751e73986d9251fbca
NS918326 <Navrajgaltee@outlook.com>
2024-04-03 16:37:11 -0400

    CRT: Implemented the article models and performed the CREATE operations

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
   root to: "articles#index"  #articles controller, index action (method)
-  get "/articles", to: "articles#index"
-
+  # get "/articles", to: "articles#index"     # get articles, routing to index action (method)
+  # get "/articles/:id", to: "articles#show"  # get article by id, routing to show action (method)
+  
+  get "up" => "rails/health#show", as: :rails_health_check
+  
+  resources :articles
   # Defines the root path route ("/")
   # root "posts#index"
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_02_183024) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_03_193908) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "cars", force: :cascade do |t|
     t.string "make"
     t.string "model"
